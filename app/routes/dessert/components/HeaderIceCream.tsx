8e951b65bdb75a0d6a3cb05e5924edb06653c388
Davonne Vigil <davonnejv@gmail.com>
2026-03-15 23:53:46 -0500

    update: header styles, and hero section for a cleaner look

--- app/routes/dessert/components/HeaderIceCream.tsx
+++ app/routes/dessert/components/HeaderIceCream.tsx
@@ -6,15 +6,13 @@ export default function Header() {
     { name: "Menu", id: 1 },
     { name: "Rewards", id: 2 },
     { name: "Gift Cards", id: 3 },
-    { name: "Careers", id: 4 },
-    { name: "Locations", id: 5 },
+    { name: "Locations", id: 4 },
   ];
 
   return (
     <header className="bg-white w-full text-black p-6 border-b-4 border-black">
       <div className="flex flex-col md:flex-row items-center justify-between">
-        <div className="w-50"></div>
-        <div className="flex flex-col items-center justify-center gap-4">
+        <div className="flex flex-col  gap-4">
           <h1 className="text-4xl md:text-5xl font-coin tracking-wider">
             Brain Freeze
           </h1>
@@ -33,17 +31,17 @@ export default function Header() {
           </ul>
         </div>
 
-        <div className="flex gap-4 font-semibold mt-4 md:mt-0">
+        <div className="flex gap-2 font-semibold mt-4 md:mt-0">
           <motion.div
             whileTap={{ scale: 0.9 }}
-            className="bg-[#13D4D4] shadow-black shadow-xl text-lg cursor-pointer border-2 border-black px-4 py-2"
+            className="bg-[#00C5FF] shadow-black shadow-xl text-md cursor-pointer border-2 border-black px-4 py-2"
           >
             Order Online
           </motion.div>
 
           <motion.div
             whileTap={{ scale: 0.9 }}
-            className="bg-[#13D4D4] shadow-black shadow-xl text-lg cursor-pointer border-2 border-black px-4 py-2"
+            className="bg-[#00C5FF] shadow-black shadow-xl text-md cursor-pointer border-2 border-black px-4 py-2"
           >
             Contact Us
           </motion.div>
--- app/routes/dessert/dessert.tsx
+++ app/routes/dessert/dessert.tsx
@@ -11,15 +11,14 @@ export default function Dessert() {
   return (
     <section className="bg-[url('images/icecream.webp')] bg-cover bg-center bg-no-repeat min-h-screen w-full">
       <HeaderIceCream />
-      <SecondHeader />
       <Card
-        className="max-w-xs md:max-w-3xl flex flex-col gap-4 mt-20 p-6 md:p-12 rounded-4xl ml-4 l md:ml-10  bg-neutral-100 shadow-md shadow-black"
+        className="max-w-xs md:max-w-2xl flex flex-col gap-4 mt-20 p-8 rounded-4xl"
         title="Its about that time! Ice Cream Time! "
-        titleClassName="font-serif font-bold text-3xl lg:text-5xl"
+        titleClassName="font-serif font-bold text-3xl lg:text-6xl"
         description={cardDescription}
         descriptionClassName="text-xl md:text-2xl tracking-wide font-quick leading-12"
         action="Check out our flavors"
-        buttonClassName="bg-white font-bold p-2 skew-y-2 font-serif mt-2 text-2xl md:w-80 lg:w-90 shadow-md shadow-black hover:scale-105 hover:bg-[#13D4D4] duration-300 cursor-pointer border-2 border-black"
+        buttonClassName="bg-white font-bold p-2 skew-y-2 font-serif mt-2 text-2xl md:w-80 lg:w-90 shadow-md shadow-black hover:scale-105 hover:bg-blue-200 duration-300 cursor-pointer border-2 border-black"
       />
     </section>
   );

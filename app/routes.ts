a3239a7fc28eced532bdcae73f0fb82ffd16cb2a
Davonne <davonnejv@gmail.com>
2026-03-15 19:44:34 -0500

    feature: header with tailwind and motion.dev

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("wings", "./routes/dessert/dessert.tsx"),
+  route("dessert", "./routes/dessert/dessert.tsx"),
 ] satisfies RouteConfig;
--- app/routes/dessert/components/HeaderIceCream.tsx
+++ app/routes/dessert/components/HeaderIceCream.tsx
@@ -1,27 +1,37 @@
+import { motion } from "motion/react";
+
 export default function () {
   const ourLinks = [
     { name: "About Us", id: 0 },
     { name: "Menu", id: 1 },
-    { name: "Reward Program", id: 2 },
+    { name: "Rewards", id: 2 },
     { name: "Gift Cards", id: 3 },
     { name: "Locations", id: 4 },
-    { name: "Merch", id: 5 },
   ];
 
   return (
-    <header className="bg-white w-full text-black p-4 border-2 border-black">
-      <div className="flex flex-col justify-between items-center gap-4">
-        <h1>The Spot</h1>
+    <header className="bg-white w-full text-black p-6 border-2 border-black flex items-center justify-between">
+      <h1 className="text-5xl font-chewy">Brain Freeze</h1>
 
-        <ul className="flex gap-10">
-          {ourLinks.map((links) => (
-            <li key={links.id}>{links.name}</li>
-          ))}
-        </ul>
+      <ul className="hidden lg:flex items-cente justify-center gap-10 text-xl font-sans tracking-widest">
+        {ourLinks.map((links) => (
+          <li key={links.id}>{links.name}</li>
+        ))}
+      </ul>
 
-        <button className="bg-[#13D4D4] border-2 border-black p-2">
+      <div className="flex gap-4">
+        <motion.div
+          whileTap={{ scale: 0.9 }}
+          className="bg-[#13D4D4] shadow-black shadow-xl text-xl cursor-pointer font-semibold border-2 border-black p-2"
+        >
           Order Online
-        </button>
+        </motion.div>
+        <motion.div
+          whileTap={{ scale: 0.9 }}
+          className="bg-[#13D4D4] shadow-black shadow-xl text-xl cursor-pointer font-semibold border-2 border-black p-2"
+        >
+          Contact Us
+        </motion.div>
       </div>
     </header>
   );

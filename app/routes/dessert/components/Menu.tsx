aab3a756e340e6fd07f5e7fa98dcc6fbc7ae5d3c
Davonne Vigil <davonnejv@gmail.com>
2026-04-01 01:22:07 -0500

    add: images for menu items, map over images and show our menu on the ui

--- app/routes/dessert/components/Menu.tsx
+++ app/routes/dessert/components/Menu.tsx
@@ -1,10 +1,22 @@
 export default function Menu() {
+  const ourMenu = [
+    { itemImage: "./images/hamburger.webp", alt: "", id: 0 },
+    { itemImage: "./images/fries.webp", alt: "", id: 1 },
+    { itemImage: "./images/ceaser.webp", alt: "", id: 2 },
+  ];
+
   return (
     <section className="flex flex-col justify-center items-center gap-4 my-20">
       <h2 className=" text-lg md:text-3xl font-quic text-center">
         ITS SO GOOD YOU GET A BRAIN FREEZE AND SOME FOOD!
       </h2>
       <p className="font-coin text-3xl">Our Menu</p>
+
+      <ul className="flex gap-20 mt-10">
+        {ourMenu.map((item) => (
+          <img src={item.itemImage} className="w-50" />
+        ))}
+      </ul>
     </section>
   );
 }

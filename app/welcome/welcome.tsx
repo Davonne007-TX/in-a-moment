c4c8b815ca5c16b8154eff9900ca20f421102877
Davonne Vigil <davonnejv@gmail.com>
2026-04-18 20:56:00 -0500

    update: home page desig with  mobile responsiveness

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,10 +1,16 @@
 export function Welcome() {
   return (
-    <section className="bg-[#0E0C0A] bg-cover min-h-screen">
+    <section className="bg-purple-600 bg-cover min-h-screen flex items-center justify-center">
       <div className="flex flex-col justify-center items-center">
-        <h1 className="text-white text-6xl mt-20 font-ber">
-          2026 Design to Code
-        </h1>
+        <div className="flex flex-col md:flex-row md:gap-10 justify-center items-center">
+          <img
+            src="/images/me.webp"
+            className="max-w-xs md:max-w-md rounded-3xl p-4"
+          />
+          <h1 className="text-white text-3xl p-2 md:text-9xl mt-20 font-quick md:ml-auto max-w-md">
+            2026 Design to <span className="font-ber"> Code</span>
+          </h1>
+        </div>
       </div>
     </section>
   );

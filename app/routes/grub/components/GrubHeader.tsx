0679b8a67c314fd2cab9f03a8f85f47edbb72bc8
Davonne Vigil <davonnejv@gmail.com>
2026-05-09 22:00:33 -0500

    add: mobile responsiveness on hero section and header

--- app/routes/grub/components/GrubHeader.tsx
+++ app/routes/grub/components/GrubHeader.tsx
@@ -17,7 +17,7 @@ export default function GrubHeader() {
   return (
     <header className="w-full">
       <div className="bg-blue-700 w-full items-center p-8">
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col md:flex-row justify-between md:items-center gap-4">
           <h1
             onClick={goToHome}
             className="text-3xl cursor-pointer md:text-5xl font-bow text-white"
@@ -25,7 +25,7 @@ export default function GrubHeader() {
             Let's Grub
           </h1>
 
-          <ul className="hidden md:flex gap-8  font-sta text-3xl text-white">
+          <ul className="hidden md:flex gap-8 font-sta text-3xl text-white">
             {myGrubHeader.map((header) => (
               <li key={header.id} className="cursor-pointer ">
                 {header.name}
@@ -33,7 +33,7 @@ export default function GrubHeader() {
             ))}
           </ul>
 
-          <div className="flex gap-8">
+          <div className="flex gap-4">
             <button className="p-2 bg-white hover:scale-105 cursor-pointer text-blue-700 font-bow">
               Order Now
             </button>
--- app/routes/grub/components/GrubHero.tsx
+++ app/routes/grub/components/GrubHero.tsx
@@ -1,6 +1,6 @@
 export default function GrubHero() {
   return (
-    <section className="w-full -mt-10 flex justify-center items-center min-h-screen">
+    <section className="w-full mt-20 md:mt-0 flex justify-center items-center min-h-screen">
       <div className="max-w-3xl flex flex-col justify-center items-center text-center gap-4 px-4">
         <p className="italic text-white text-2xl">
           Est. 1993 in Los Angeles, CA
@@ -11,18 +11,18 @@ export default function GrubHero() {
           <span className="text-blue-600">grub</span> !
         </h1>
 
-        <p className="tracking-widest leading-12 text-3xl  text-white p-2">
+        <p className="tracking-widest leading-12 text-xl md:text-3xl  text-white p-2">
           Imagine being on the beach, hanging out with friends, and all of
           sudden you get hungry. Let's Grub is right on the beach for classic
           summer favorites! Lets eat!
         </p>
 
-        <div className="flex gap-8">
+        <div className="flex flex-col md:flex-row gap-8">
           <button className="p-2 bg-white hover:scale-105 cursor-pointer text-blue-700 font-bow text-lg">
             Make a Reservation
           </button>
 
-          <button className="p-2 bg-white hover:scale-105 cursor-pointer text-blue-700 font-bow text-lg">
+          <button className="p-2 bg-white hover:scale-105 cursor-pointer text-blue-700 font-bow text-md text-lg">
             Explore Menu
           </button>
         </div>
